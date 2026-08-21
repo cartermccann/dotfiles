d9e9a0b3ac49bb1730dc6534679ae502ab11a1a9
cartermccann <cjmccann00@gmail.com>
2026-08-20 18:59:15 -0600

    feat(kronos): configure korgo mini connection

--- hosts/kronos/configuration.nix
+++ hosts/kronos/configuration.nix
@@ -28,6 +28,27 @@
     ../../modules/oom-protection.nix
   ];
 
+  users.users.cjm.uid = 1000;
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/korgo-ssh-secrets 0700 cjm users - -"
+  ];
+
+  services.korgo-ssh-client = {
+    enable = true;
+    package = korgo.packages.x86_64-linux.korgo-ssh-client;
+    user = "cjm";
+    group = "users";
+    uid = 1000;
+    miniAddress = "100.92.118.102";
+    port = 22;
+    identityFile = "/var/lib/korgo-ssh-secrets/identity";
+    knownHostsFile = "/var/lib/korgo-ssh-secrets/known_hosts";
+    knownHostsSha256 = "22027cbf6896e265d62ebf67f61914269698368b973ba191032192261f1e152d";
+    hostKeyFingerprint = "SHA256:QqLF3mcRl70gKZY//Kxlrb814fmUGXA8sAZN/UOcIEc";
+    waylandDisplay = "wayland-1";
+  };
+
   networking.hostName = "kronos";
 
   # Larger console font for Ly login manager on high-res display
